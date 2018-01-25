488d2ef8088d1926190eab4ac35416ee48b13dce
varuneagle555 <varunroyeagle@outlook.com>
2018-01-25 10:09:34 -0500

    Creating core

---
+++ VisLabCore/Assets/Scripts/GlobeControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GlobeControl : MonoBehaviour
+{
+    public float x;
+    public float y;
+    public float z;
+    public GameObject obj;
+
+    private Vector3 position;
+
+    void Start()
+    {
+        position = new Vector3(x, y, z);
+    }
+
+    void Update()
+    {
+        position = new Vector3(x, y, z);   
+    }
+}
