c0911907f4b75499ce7d6952c6bcbaae8edd0aeb
varuneagle555 <varunroyeagle@outlook.com>
2018-02-03 13:21:05 -0500

    Removed Spatial Mapping

---
+++ VisLabCore/Assets/Scripts/Core/CoreControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreControl : MonoBehaviour {
+
+    private Transform transform;
+
+    private Vector3 ROTATION = new Vector3(0.5f, 0.5f, 0.5f);
+
+	// Use this for initialization
+	void Start () {
+        transform = this.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(ROTATION);
+	}
+}
