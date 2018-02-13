e4bb7dc8d3ab4d80d50c10122b8716ae9d0d4e2b
varuneagle555 <varunroyeagle@outlook.com>
2018-02-13 10:36:04 -0500

    Adding Scripts for Objects to be Rendered

--- VisLabCore/Assets/Scripts/TerraVis/GlobeControl.cs
+++ VisLabCore/Assets/Scripts/TerraVis/GlobeControl.cs
@@ -7,15 +7,18 @@ public class GlobeControl : MonoBehaviour
     public float z;
     public GameObject obj;
 
-    private Vector3 position;
+    private Vector3 ROTATION_VECTOR =  new Vector3(0,0.5f,0);
 
     void Start()
     {
-        position = new Vector3(x, y, z);
+
     }
 
     void Update()
     {
-        position = new Vector3(x, y, z);   
+        this.GetComponent<Transform>().Rotate(ROTATION_VECTOR);  
     }
+
+    //Implement translation of scene coordinates to Earth coordinates
+
 }
